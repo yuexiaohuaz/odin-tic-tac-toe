7b212e93fa7b1cde16cb589e910e2a94df6140da
Yue <190688943+yuexiaohuaz@users.noreply.github.com>
2025-02-24 00:47:54 -0600

    Created css customization.

--- javascript.js
+++ javascript.js
@@ -1,43 +1,3 @@
-//global function with all the ifno, and then a function call at end
-//create the gameboard with cells corresponding with 1-9. 
-//to place something, the palyers type in a number from 1-9.
-//computer will use random to select a square to place their piece
-//computer function triggers after player function, wiht a small delay?
-//in game function, once a win is detected (an object)
-/* In gameboard function, generate teh grid. create a method
-that the ui can use to get the board
-then we have another function that will take the number the player chooses
-and put a mark for that square there. if it already has a number there, return 
-"that does not work".
-also a printboard function that prints board to UI
-const printBoard = () => {
-    const boardWithCellVallues = board.map((row) => row.map((cell) => cell.getValue()));
-    console.log(boardWithCellValues);
-    }
-then return the interface. return the entire board, what happens after
-entering anumber, then print the baord.
-*/ 
-/* In each cell, have the number inside it be a value and changed whenever 
-the palyer enteres a number to the player's value. also have a getter that 
-returns the value inside the cell.
-the cell method should return the getting value and adding a token.
- */
-/*
-For the actual game,
-first create two players (at first it will have no user input)
-create a baord object using gameboard and give tokens to the players. store in array?
-let the active player be the first one (say let activePlayer = players[0])
-then create a method that gets the active player
-add a method that switches whose turn it is (use ? : format)
-every round repreint the board lol using a method
-
-create a function for actually playing a round--where the parameter
-is the number that you're marking the spot. log where you drop and then
-use the method from the board
-
- */
-//AT FIRST IGNORE USER INPUT. you can callf ucntions and pass argments to them to play the game
-
 function gameBoard() {
     let board = [];
     const rows = 3;
@@ -110,12 +70,11 @@ function playGame() {
         ]
         for(const [a, b, c] of winConditions) {
             if (boardElements[a] == boardElements[b] && boardElements[b] == boardElements[c] && boardElements[a] != 0) {
-                console.log(`${getActivePlayer().name} has won!`);
-                return;
+                return `${getActivePlayer().name} has won!`;
             } 
         }
         if (boardElements.filter(item => item == 0).length == 0) {
-            console.log("Tie!");
+            return "Tie!";
         }
     }
     const playRound = (piecePlaced) => {
@@ -127,19 +86,19 @@ function playGame() {
         
     }
     printNewRound();
-    return {playRound, getActivePlayer};
+    return {playRound, getActivePlayer, checkGameWon};
 }
 
-// function screenController() {
-//     const game = playGame();
-//     const updateScreen = () => {
-//         //sets .board's div to empty
-//         //gets board from game controller and active player
-//         //renders the player's turn (informs them on the turn div)
-//         //render each grid square on the DOM. each cell is a button.
-//     }
-//     const addMark = () => {
-//         //trigger this on event listener
-//     }
-// }
+function screenController() {
+    const game = playGame();
+    const updateScreen = () => {
+        //sets .board's div to empty
+        //gets board from game controller and active player
+        //renders the player's turn (informs them on the turn div)
+        //render each grid square on the DOM. each cell is a button.
+    }
+    const addMark = () => {
+        //trigger this on event listener
+    }
+}
 
